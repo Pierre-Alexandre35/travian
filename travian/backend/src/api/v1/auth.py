19da734dc20b63888e7425255896baca195134c3
Pierre-Alexandre35 <46579114+pamousset75@users.noreply.github.com>
2022-03-27 21:51:46 +0200

    (update) crud operation

--- travian/backend/src/api/v1/auth.py
+++ travian/backend/src/api/v1/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from src.db.crud import create_user
-from src.db.schemas.user import *
+from src.db.crud.user import create_user
+from src.db.schemas.user import User
 auth_router = r = APIRouter()
 
 @r.post("/register")
@@ -9,13 +9,8 @@ async def register(user: User):
 
 
 @r.post("/users", response_model=User, response_model_exclude_none=True)
-async def user_create(
-    request: Request,
-    user: UserCreate,
-    db=Depends(get_db),
-    current_user=Depends(get_current_active_superuser),
-):
+async def user_create(user: User):
     """
     Create a new user
     """
-    return create_user(db, user)
+    return create_user(user)
