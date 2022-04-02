144c63ebe1b1cf914bc74219a3547d332178a232
Pierre-Alexandre35 <46579114+pamousset75@users.noreply.github.com>
2022-04-02 09:25:39 +0200

    (test

--- travian/backend/src/api/v1/auth.py
+++ travian/backend/src/api/v1/auth.py
@@ -1,14 +1,9 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends
 from src.db.crud.user import create_user
-from src.db.schemas.user import User, UserCreate
+from src.db.schemas.user import UserCreate
 from src.db.utils import get_db
 
 auth_router = r = APIRouter()
-
-
-@r.post("/register")
-async def register(user: User):
-    return user.username
 
 
 @r.get("/aa", response_model=UserCreate, response_model_exclude_none=True)
