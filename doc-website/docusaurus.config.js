05909a35a7b67a3176b132155efb11366247050c
Pierre-Alexandre35 <46579114+pamousset75@users.noreply.github.com>
2022-04-29 20:08:45 +0200

    (update) script to move build files into the root docs folder for github pages

--- doc-website/docusaurus.config.js
+++ doc-website/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/docs',
+  baseUrl: '/docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
