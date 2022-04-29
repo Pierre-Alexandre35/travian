cb00feb7e604a7476bac88874e6716028563e7b2
Pierre-Alexandre35 <46579114+pamousset75@users.noreply.github.com>
2022-04-29 20:22:35 +0200

    (update) script to move build files into the root docs folder for github pages

--- doc-website/docusaurus.config.js
+++ doc-website/docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/travian_browser_game/',
+  url: 'https://pierre-alexandre35.github.io/',
+  baseUrl: '/travian_browser_game/docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
