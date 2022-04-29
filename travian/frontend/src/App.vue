f79fd594cf0455785c63b6b0cbdaa728b4d64493
Pierre-Alexandre35 <46579114+pamousset75@users.noreply.github.com>
2022-04-29 17:50:55 +0200

    (major) working docker-compose

--- travian/frontend/src/App.vue
+++ travian/frontend/src/App.vue
@@ -1,17 +1,26 @@
 <template>
-  <HomePage msg="Welcome to Your Vue.js App" />
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg />
 </template>
-<style lang="scss">
-@import "./styles/main.scss"; // $text-primary would be defined in that file
-</style>
+
 <script>
-import HomePage from "./components/HomePage.vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "App",
   components: {
-    HomePage,
+    HelloWorld,
   },
 };
 </script>
 
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
