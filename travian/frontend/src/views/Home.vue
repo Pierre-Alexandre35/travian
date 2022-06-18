7d591b2d47c528919e0d58c2b1e28909f0f42b89
Pierre-Alexandre35 <46579114+pamousset75@users.noreply.github.com>
2022-06-18 17:32:34 +0200

    (major) vue3 routing template

---
+++ travian/frontend/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="This is the Home Page" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
