39902521a3d6a7fac59144a6b9efec3819c7cd19
Pierre-Alexandre35 <46579114+Pierre-Alexandre35@users.noreply.github.com>
2025-08-17 22:16:12 +0200

    yip

--- iac/bootstrap/ci_sa.tf
+++ iac/bootstrap/ci_sa.tf
@@ -10,9 +10,21 @@ resource "google_service_account_iam_member" "ci_wif_impersonation" {
   member             = "principalSet://iam.googleapis.com/${module.github_wif.github_pool_id}/*"
 }
 
-resource "google_project_iam_member" "ci_can_deploy_run" {
+resource "google_project_iam_member" "ci_deployer_run_admin" {
   project = var.project
   role    = "roles/run.admin"
+  member  = "serviceAccount:${google_service_account.ci_deployer.email}"
+}
+
+resource "google_project_iam_member" "ci_deployer_artifact_admin" {
+  project = var.project
+  role    = "roles/artifactregistry.admin"
+  member  = "serviceAccount:${google_service_account.ci_deployer.email}"
+}
+
+resource "google_project_iam_member" "ci_deployer_sa_user" {
+  project = var.project
+  role    = "roles/iam.serviceAccountUser"
   member  = "serviceAccount:${google_service_account.ci_deployer.email}"
 }
 
@@ -22,8 +34,8 @@ resource "google_project_iam_member" "ci_can_push_to_ar" {
   member  = "serviceAccount:${google_service_account.ci_deployer.email}"
 }
 
-resource "google_project_iam_member" "ci_can_use_sa" {
-  project = var.project
-  role    = "roles/iam.serviceAccountUser"
-  member  = "serviceAccount:${google_service_account.ci_deployer.email}"
+resource "google_storage_bucket_iam_member" "ci_can_access_tfstate" {
+  bucket = "tfstate-vallorium-eu"
+  role   = "roles/storage.admin"
+  member = "serviceAccount:${google_service_account.ci_deployer.email}"
 }
